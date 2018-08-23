fe491773676dd16ea53a86209086e9fd6c8d2939
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-23 11:24:26 +1000

    Updated RenEx sdk

--- renex_sdk.go
+++ renex_sdk.go
@@ -1,18 +1,43 @@
 package renex
 
 import (
+	"github.com/republicprotocol/renex-sdk-go/adapter/client"
+	fundsAdapter "github.com/republicprotocol/renex-sdk-go/adapter/funds"
+	obAdapter "github.com/republicprotocol/renex-sdk-go/adapter/orderbook"
+	"github.com/republicprotocol/renex-sdk-go/adapter/trader"
+
 	"github.com/republicprotocol/renex-sdk-go/core/funds"
 	"github.com/republicprotocol/renex-sdk-go/core/orderbook"
 )
 
-type renEx struct {
+type RenEx struct {
+	orderbook.Orderbook
+	funds.Funds
 }
 
-type RenEx interface {
-	orderbook.Input
-	funds.Input
-}
+func NewRenEx(ingressAddress, configPath, keystorePath, passphrase string) (RenEx, error) {
+	newTrader, err := trader.NewTrader(keystorePath, passphrase)
+	if err != nil {
+		return RenEx{}, err
+	}
 
-func NewRenEx() RenEx {
-	return nil
+	newClient, err := client.NewClient(configPath)
+	if err != nil {
+		return RenEx{}, err
+	}
+
+	fAdapter, err := fundsAdapter.NewAdapter(ingressAddress, newClient, newTrader)
+	if err != nil {
+		return RenEx{}, err
+	}
+
+	oAdapter, err := obAdapter.NewAdapter(ingressAddress, newClient, newTrader)
+	if err != nil {
+		return RenEx{}, err
+	}
+
+	return RenEx{
+		orderbook.NewService(oAdapter),
+		funds.NewService(fAdapter),
+	}, nil
 }
