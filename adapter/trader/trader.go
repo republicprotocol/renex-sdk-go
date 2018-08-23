fbd1853c5594d4c728aa3779c9bb1753e4c628eb
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-23 11:24:54 +1000

    Updated trader Adapter

--- adapter/trader/trader.go
+++ adapter/trader/trader.go
@@ -1,34 +1,54 @@
 package trader
 
 import (
+	"crypto/rand"
+	"io/ioutil"
 	"os"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type trader struct {
-	path string
+	*keystore.Key
 }
 
 // Trader represents an individual entity that opens orders.
 // Traders can be either honest participants, malicious
 // or even participants with insufficient funds.
 type Trader interface {
-	keystore crypto.Keystore
+	Sign([]byte) ([]byte, error)
+	TransactOpts() *bind.TransactOpts
+	Address() common.Address
 }
 
-func New(path string) Trader {
-	return &trader{
-		path: path,
-	}
-}
-
-func (t *trader) GetTransactOpts(passphrase string) (*bind.TransactOpts, error) {
-	keyin, err := os.Open(t.path)
-	defer keyin.Close()
+func NewTrader(path string, passphrase string) (Trader, error) {
+	keyin, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewTransactor(keyin, passphrase)
+	json, err := ioutil.ReadAll(keyin)
+	if err != nil {
+		return nil, err
+	}
+	key, err := keystore.DecryptKey(json, passphrase)
+	if err != nil {
+		return nil, err
+	}
+	return &trader{
+		key,
+	}, nil
+}
+
+func (t *trader) TransactOpts() *bind.TransactOpts {
+	return bind.NewKeyedTransactor(t.PrivateKey)
+}
+
+func (t *trader) Address() common.Address {
+	return t.Key.Address
+}
+
+func (t *trader) Sign(data []byte) ([]byte, error) {
+	return t.PrivateKey.Sign(rand.Reader, data, nil)
 }
