aa5a9ad71d0361162bd12abaff5ea420d1d62f4c
Susruth  Nadimpalli <susruth@republicprotocol.com>
2018-08-23 11:25:08 +1000

    Updated funds service

--- core/funds/funds.go
+++ core/funds/funds.go
@@ -1,31 +1,62 @@
 package funds
 
 import (
+	"fmt"
 	"math/big"
-
-	"github.com/republicprotocol/renex-sdk-go/adapter/funds"
 )
 
+type IdempotentKey [32]byte
+
 type service struct {
-	funds.Adapter
+	Adapter
 }
 
-type Service interface {
-	Deposit(token uint32, value *big.Int)
-	Withdraw(token uint32, value *big.Int, forced bool, key *IdempotentKey)
+type Adapter interface {
+	RequestDeposit(tokenCode uint32, value *big.Int) error
+	RequestWithdrawalSignature(tokenCode uint32, value *big.Int) ([]byte, error)
+	RequestWithdrawalWithSignature(tokenCode uint32, value *big.Int, signature []byte) error
+	RequestWithdrawalFailSafe(tokenCode uint32, value *big.Int) error
+	RequestWithdrawalFailSafeTrigger(tokenCode uint32) (*IdempotentKey, error)
+	CheckStatus(key *IdempotentKey) uint8
 }
 
-func (service *service) Withdraw(token uint32, value *big.Int, forced bool, key *IdempotentKey) error {
-	sig, err := service.RequestWithdrawalSignature(token, value);
-	if err != nil {
-		if !forced {
-			return err
-		}
-		if err := service.RequestWithdrawalFailSafeTrigger(); err != nil {
-			return err
-		}
-		if err := service.RequestWithdrawalFailSafe(tokenCode uint32, value *big.Int); err != nil {
-			return err
-		}
+type Funds interface {
+	Deposit(token uint32, value *big.Int) error
+	Withdraw(token uint32, value *big.Int, forced bool, key *IdempotentKey) (*IdempotentKey, error)
+}
+
+func NewService(adapter Adapter) Funds {
+	return &service{
+		adapter,
 	}
+}
+
+func (service *service) Withdraw(token uint32, value *big.Int, forced bool, key *IdempotentKey) (*IdempotentKey, error) {
+	switch service.CheckStatus(key) {
+	case 0:
+		sig, err := service.RequestWithdrawalSignature(token, value)
+		if err != nil {
+			if !forced {
+				return nil, err
+			}
+			failSafeKey, err := service.RequestWithdrawalFailSafeTrigger(token)
+			if err != nil {
+				return nil, err
+			}
+			return failSafeKey, nil
+		}
+		return nil, service.RequestWithdrawalWithSignature(token, value, sig)
+	case 1:
+		return key, fmt.Errorf("Transaction Pending")
+	case 2:
+		return key, fmt.Errorf("Time left to trigger withdrawal without signature")
+	case 3:
+		return key, service.RequestWithdrawalFailSafe(token, value)
+	default:
+		return key, fmt.Errorf("Corrupted Idempotent Key")
+	}
+}
+
+func (service *service) Deposit(tokenCode uint32, value *big.Int) error {
+	return service.RequestDeposit(tokenCode, value)
 }
