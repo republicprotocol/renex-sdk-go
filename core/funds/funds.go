ef955baf894e71f5860b0aa344516124092f7ee8
tok-kkk <yunshi@republicprotocol.com>
2018-09-11 14:27:14 +1000

    dep ensure and update tokens

--- core/funds/funds.go
+++ core/funds/funds.go
@@ -96,9 +96,11 @@ func (service *service) Transfer(address string, tokenCode order.Token, value *b
 		return service.TransferERC20(address, tokenCode, value)
 	case order.TokenDGX:
 		return service.TransferERC20(address, tokenCode, value)
-	case order.TokenABC:
+	case order.TokenTUSD:
 		return service.TransferERC20(address, tokenCode, value)
-	case order.TokenXYZ:
+	case order.TokenZRX:
+		return service.TransferERC20(address, tokenCode, value)
+	case order.TokenOMG:
 		return service.TransferERC20(address, tokenCode, value)
 	case order.TokenETH:
 		return service.TransferEth(address, value)
@@ -113,9 +115,11 @@ func (service *service) Balance(tokenCode order.Token) (*big.Int, error) {
 		return service.BalanceErc20(tokenCode)
 	case order.TokenDGX:
 		return service.BalanceErc20(tokenCode)
-	case order.TokenABC:
+	case order.TokenTUSD:
 		return service.BalanceErc20(tokenCode)
-	case order.TokenXYZ:
+	case order.TokenZRX:
+		return service.BalanceErc20(tokenCode)
+	case order.TokenOMG:
 		return service.BalanceErc20(tokenCode)
 	case order.TokenETH:
 		return service.BalanceEth()
