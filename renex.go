621cee59505c382091287481bca8d62beabeaaf0
Jaz Gulati <jaskaran.g15@gmail.com>
2018-10-22 15:14:38 +1100

    renex: cleaned up NewRenex methods

--- renex.go
+++ renex.go
@@ -13,8 +13,6 @@ import (
 	obAdapter "github.com/republicprotocol/renex-sdk-go/adapter/orderbook"
 	"github.com/republicprotocol/renex-sdk-go/adapter/store"
 	"github.com/republicprotocol/renex-sdk-go/adapter/trader"
-	"github.com/republicprotocol/republic-go/crypto"
-
 	"github.com/republicprotocol/renex-sdk-go/core/funds"
 	"github.com/republicprotocol/renex-sdk-go/core/orderbook"
 )
@@ -30,67 +28,32 @@ func NewRenEx(network, keystorePath, passphrase string) (RenEx, error) {
 	if err != nil {
 		return RenEx{}, err
 	}
-	return newRenEx(network, newTrader)
-}
-
-func NewRenExWithPrivKey(network string, privKey *ecdsa.PrivateKey) (RenEx, error) {
-	return newRenEx(network, trader.NewTraderFromPrivateKey(privKey))
-}
-
-func newRenEx(network string, t trader.Trader) (RenEx, error) {
-
-	ingressAddress := fmt.Sprintf("https://renex-ingress-%s.herokuapp.com", network)
-
 	newClient, err := client.NewClient(network)
 	if err != nil {
 		return RenEx{}, err
 	}
-
-	randomDBID := make([]byte, 32)
-	if _, err := rand.Read(randomDBID); err != nil {
-		return RenEx{}, err
-	}
-
-	newStoreAdapter, err := leveldb.NewLDBStore(os.Getenv("HOME") + fmt.Sprintf("/.renex/db%s", hex.EncodeToString(randomDBID)))
-	if err != nil {
-		return RenEx{}, err
-	}
-
-	newStore := store.NewStore(newStoreAdapter)
-	if err != nil {
-		return RenEx{}, err
-	}
-
-	fAdapter, err := fundsAdapter.NewAdapter(ingressAddress, newClient, t, newStore)
-	if err != nil {
-		return RenEx{}, err
-	}
-
-	fService := funds.NewService(fAdapter)
-
-	oAdapter, err := obAdapter.NewAdapter(ingressAddress, newClient, t, fService, newStore, network)
-	if err != nil {
-		return RenEx{}, err
-	}
-
-	return RenEx{
-		orderbook.NewService(oAdapter),
-		fService,
-		newClient,
-	}, nil
+	return newRenEx(network, newTrader, newClient)
 }
 
-func NewRenExFromNetwork(network string, clientNetwork client.Network, keystore crypto.Keystore) (RenEx, error) {
-	ingressAddress := fmt.Sprintf("https://renex-ingress-%s.herokuapp.com", network)
-	newTrader, err := trader.NewTraderFromKeystore(keystore)
+func NewRenExWithPrivKey(network string, privKey *ecdsa.PrivateKey) (RenEx, error) {
+	newClient, err := client.NewClient(network)
 	if err != nil {
 		return RenEx{}, err
 	}
+	return newRenEx(network, trader.NewTraderFromPrivateKey(privKey), newClient)
+}
 
+func NewRenExWithNetwork(network string, clientNetwork client.Network, privKey *ecdsa.PrivateKey) (RenEx, error) {
+	newTrader := trader.NewTraderFromPrivateKey(privKey)
 	newClient, err := client.NewClientFromNetwork(clientNetwork)
 	if err != nil {
 		return RenEx{}, err
 	}
+	return newRenEx(network, newTrader, newClient)
+}
+
+func newRenEx(network string, t trader.Trader, c client.Client) (RenEx, error) {
+	ingressAddress := fmt.Sprintf("https://renex-ingress-%s.herokuapp.com", network)
 
 	randomDBID := make([]byte, 32)
 	if _, err := rand.Read(randomDBID); err != nil {
@@ -107,14 +70,14 @@ func NewRenExFromNetwork(network string, clientNetwork client.Network, keystore 
 		return RenEx{}, err
 	}
 
-	fAdapter, err := fundsAdapter.NewAdapter(ingressAddress, newClient, newTrader, newStore)
+	fAdapter, err := fundsAdapter.NewAdapter(ingressAddress, c, t, newStore)
 	if err != nil {
 		return RenEx{}, err
 	}
 
 	fService := funds.NewService(fAdapter)
 
-	oAdapter, err := obAdapter.NewAdapter(ingressAddress, newClient, newTrader, fService, newStore, network)
+	oAdapter, err := obAdapter.NewAdapter(ingressAddress, c, t, fService, newStore, network)
 	if err != nil {
 		return RenEx{}, err
 	}
@@ -122,7 +85,6 @@ func NewRenExFromNetwork(network string, clientNetwork client.Network, keystore 
 	return RenEx{
 		orderbook.NewService(oAdapter),
 		fService,
-		newClient,
+		c,
 	}, nil
-
 }
